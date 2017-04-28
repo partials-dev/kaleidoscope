9072a3e98301dba5714c2881b2a73debef1c334f
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2017-04-28 18:34:52 -0400

    Remove image source input.

--- src/components/work/Controls.js
+++ src/components/work/Controls.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import Showable from '../look/Showable'
 import { connect } from 'react-redux'
-import ImageSourceInput from './ImageSourceInput'
-import ChangeImageButton from './ChangeImageButton'
 import ChangeSpeedButton from './ChangeSpeedButton'
 import XPanSpeedInput from './XPanSpeedInput'
 import YPanSpeedInput from './YPanSpeedInput'
 
 const mapStateToProps = state => {
   const children = <div>
-    <ImageSourceInput /><ChangeImageButton />
     <XPanSpeedInput /><YPanSpeedInput /><ChangeSpeedButton />
   </div>
-
   return {
     children,
     show: state.ui.showControls
